1281eb701003a66b17c1462116d1c81e5e42fd44
Michael Orr <michael@cloudspace.com>
2011-01-26 02:04:56 -0500

    logo fixes, typos and syntax, oh my

--- lib/template/recipes/logo.rb
+++ lib/template/recipes/logo.rb
@@ -1,14 +1,4 @@
 post_bundler_strategies << lambda do 
   #why don't we download this to the server and put it in the public folder?
-  
-  
-  
-  
-  application_layout_modification = <<-END
-
-  %img{:src => @template_options[:logo_image]}
-
-END
-
-  inject_into_file 'app/views/layouts/application.html.haml', application_layout_modification,:before => '%p.notice= notice'
+  inject_into_file 'app/views/layouts/application.html.haml', "  %img{:src => \"#{@template_options[:logo_image]}\"}\n",:before => '  %p.notice= notice'
 end
